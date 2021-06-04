5c5a1aeb45d32ea2c2e4a957c6cf5f27f0732511
Samelena <lyly-miranda@yandex.ru>
2021-06-04 12:51:32 +0300

    Я честно пыталась сделать это задание, но это оказалось выше моих умственных способностей...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,6 @@ public class Main {
     public static void main(String[] args) {
         UserInterfaceView userInterfaceView = new UserInterfaceView();
         userInterfaceView.runInterface();
+        System.out.println("Я не знаю как сделать это задание");
+    }
 }
